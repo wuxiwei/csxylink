a9d83369f4732a21d7f33dc014204d2ace328895
wxw <wxw_it@163.com>
2016-03-25 23:03:59 +0800

    新学期更新

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 var config = {
   port: 5000,
-  term_schedule: 15,    //查课表看下拉框第几个,每学期维护
+  term_schedule: 16,    //查课表看下拉框第几个,每学期维护
   term_grade : new Array(     //每学期维护
     "2008-2009学年第1学期",
     "2008-2009学年第2学期",
